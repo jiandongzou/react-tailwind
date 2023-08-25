7478c10a0c044fa115dd7c951720631592f3d739
zoujiandong <10130823232@qq.com>
2023-08-25 11:39:17 +0800

    8.25

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+const themes = ["theme1", "theme2", "theme3"];
+import React, { useState } from "react";
 function App() {
-  
+
+  const [theme, setTheme] = useState("theme1");
   return (
-    <>
-      <div className="container mx-auto bg-gray-200 hover:bg-green-50 rounded-xl shadow border p-8 m-10">
+    <div className={`theme-${theme} w-screen h-screen bg-background`}>
+      <div className="container mx-auto bg-gray-200 hover:bg-green-50 rounded-xl shadow border p-8 ml-2 mr-2">
       <p className="text-gray-700 font-bold mb-5 text-[18px]">
         Welcome!
       </p>
@@ -13,8 +16,16 @@ function App() {
         我是自定义嵌套颜色
       </p>
     </div>
-    </>
+    <div className="container flex flex-col justify-center items-center">
+      <p className="text-center">当前主题:{theme}</p>
+      <div className=" flex justify-around w-[340px] mt-2">
+      {themes.map((item,index)=>{
+        return <button key={index} className="border rounded p-2 bg-secondary text-primary" onClick={()=>setTheme(item)}>{item}</button>
+      })}
+        
+      </div>
+    </div>
+    </div>
   )
 }
-
 export default App
